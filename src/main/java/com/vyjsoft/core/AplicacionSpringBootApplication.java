585cd46cac24e5c3445190106f87fa3f1fa6083a
Jose Gomez <josegelimergomez@gmail.com>
2020-11-17 16:00:04 -0300

    primer commit

---
+++ src/main/java/com/vyjsoft/core/AplicacionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.vyjsoft.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionSpringBootApplication.class, args);
+	}
+
+}
